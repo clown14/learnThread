0d8f5fc66afe6adaeb962b741f3859cb5d582a21
clown <785613198@qq.com>
2020-10-06 13:09:35 +0800

    Singleton DCL

---
+++ atguigu/src/com/atguigu/interview/thread/SingletonDemo.java
@@ -0,0 +1,35 @@
+package com.atguigu.interview.thread;
+
+/**
+ * @program: Thread
+ * @author: onion
+ * @create: 2020-10-05 14:04
+ **/
+
+public class SingletonDemo {
+    private static SingletonDemo singletonDemo=null;
+    private SingletonDemo(){
+        System.out.println(Thread.currentThread().getName()+"\t 我是构造方法");
+    }
+    //DCL模式 Double Check Lock 双端检索机制：在加锁前后都进行判断
+    public static SingletonDemo getInstance(){
+        if (singletonDemo==null){
+            synchronized (SingletonDemo.class){
+                if (singletonDemo==null){
+                    singletonDemo=new SingletonDemo();
+                }
+            }
+        }
+        return singletonDemo;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->{
+                SingletonDemo.getInstance();
+            },String.valueOf(i+1)).start();
+        }
+    }
+}
+
+    
